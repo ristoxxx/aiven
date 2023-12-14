609c03ffeda15b2a9eba322eee47a90807ef9d29
Risto Lähteenkorva <lahteri@gmail.com>
2023-12-14 21:00:06 +0000

    owner added

--- src/main/java/io/aiven/dataworkshop/model/Owner.java
+++ src/main/java/io/aiven/dataworkshop/model/Owner.java
@@ -1,22 +1,18 @@
 package io.aiven.dataworkshop.model;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
 
 @Entity
-public class Cat {
-
+public class Owner {
+    
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Integer id;
 
     private String name;
-    
-    private int age;
 
     public Integer getId() {
         return id;
@@ -33,25 +29,5 @@ public class Cat {
     public void setName(String name) {
         this.name = name;
     }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
     
-    @ManyToOne(cascade = CascadeType.PERSIST)
-    Owner owner;
-
-    public Owner getOwner() {
-        return owner;
-    }
-
-    public void setOwner(Owner owner) {
-        this.owner = owner;
-    }
-
-
 }
